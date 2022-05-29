3fbb021362c2cf2043119bbcd9efb1cc8a7ff75f
AmrAbdelgalil <93317873+AmrAbdelgalil@users.noreply.github.com>
2022-05-29 14:45:37 +0200

    Start project

---
+++ e-commerce-app/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>E-Commerce App</h1>
+      <button className="btn btn-primary btn-lg">Login</button>
+      <button className="btn btn-secondary btn-lg">Signup</button>
+    </main>
+  );
+}
+
+export default App;
