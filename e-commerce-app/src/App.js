2e6a08ef7e87202dfec17d13fb203f53cb645ef5
Bishoy Osama <engbishoyosama@gmail.com>
2022-06-14 22:08:49 +0200

    fix: rename menu to products

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contactus from "./pages/Contactus";
 import Login from "./pages/Login";
-import Menu from "./pages/Menu";
+import Products from "./pages/Products";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
 import Footer from "./components/Footer";
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/menu" element={<Menu />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/contactus" element={<Contactus />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
