9b42fb2c01488a867f3495cfeed497aa8c0aad26
AmrAbdelgalil <93317873+AmrAbdelgalil@users.noreply.github.com>
2022-06-16 10:47:48 +0200

    feat: Building Error 404 Page and adding Single Product Page

---
+++ e-commerce-app/src/pages/SingleProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleProduct = () => {
+  return (
+    <section className="section-center">
+      <h1>SingleProduct</h1>
+    </section>
+  );
+};
+
+export default SingleProduct;
